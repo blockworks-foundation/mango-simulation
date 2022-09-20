c5e21c5328083e1e88c2ee39826fc12ac3fd2045
Godmode Galactus <godmodegalactus@gmail.com>
2022-09-20 21:35:49 +0200

    Removing unnecessary file

--- bench_tps_run.sh
+++
@@ -1,6 +0,0 @@
-cargo run \
---bin solana-bench-mango \
--- -u https://api.testnet.rpcpool.com/dfeb84a5-7fe8-4783-baf9-60cca0babbc7 \
---identity ~/devnet.json \
---accounts ~/accounts20.json  \
---mango ~/ids.json --duration 10 -q 2 --transaction_save_file tlog.txt
